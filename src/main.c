eda136aa5b5f3203560c080b7873bdbdc72a69f5
Keaton Lind <Keaton.lind05@gmail.com>
2026-04-11 18:01:54 -0600

    changed glfw to precompiled binaries

--- src/main.c
+++ src/main.c
@@ -1,5 +1,47 @@
+
+#include <GLFW/glfw3.h>
 #include <stdio.h>
-int main()
+
+void error_callback(int error, const char* description)
 {
-  printf("hallo\n");
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+int main(void)
+{
+    printf("progarm initialized\n");
+    GLFWwindow* window;
+
+    glfwSetErrorCallback(error_callback);
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
